aa63625bd5cccf5b2cca0c7297785c49fdcfe2ae
Naga-Tharun <nagatharunmakkena@gmail.com>
2023-06-27 11:02:03 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Naga Tharun Makkena on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
